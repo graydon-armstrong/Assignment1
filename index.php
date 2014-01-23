86bbdb13ba7481bc4a3259f51bde489e4a735d0b
Graydon Armstrong <seeds77222@hotmail.com>
2014-01-23 18:37:50 -0500

    Initial Website Commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Graydons Web Design</title>
+		<!-- <link rel="stylesheet" type="text/css" href="css/styles.css" />-->
+		<link rel="stylesheet" href="css/foundation.css" />
+		<script src="js/modernizr.js"></script>
+	</head>
+	
+	<body>
+	
+	<div id= "main_layout">	
+		
+		<div class="row">
+			<div class="large-12 columns">
+				<nav class="top-bar" data-topbar>
+					<ul class="title-area">
+						<li class="name">
+							<h1><a href="#">Graydons Web Design</a></h1>
+						</li>
+						<li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+					</ul>
+								
+					<section class="top-bar-section">
+					<ul class="right">
+						<li class="active"><a href="#">Home</a></li>
+						<li class="active"><a href="#">About Me</a></li>
+						<li class="active"><a href="#">Projects</a></li>
+						<li class="active"><a href="#">Services</a></li>
+						<li class="active"><a href="#">Github</a></li>
+						<li class="active"><a href="#">Contact Me</a></li>
+					</ul>
+					</section>
+				</nav>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="large-12 columns">
+				<h1>Graydons Web Design</h1>
+			</div>
+		</div>
+
+		<div id="content">
+			<p>Here is an example homepage.</p>
+		</div>
+
+		<div id="footer">
+			<p>&copy Graydons Web Design</p>
+		</div>
+	</div>
+	
+	<script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+
+	</body>
+
+</html>
