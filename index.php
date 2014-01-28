a5d36d3f9cc647003086aaeb1fc653252e155a19
Graydon Armstrong <seeds77222@hotmail.com>
2014-01-28 00:58:24 -0500

    Added a Orbit Slider, more work button, connect with me section. Also added style sheet for non foundation styles

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 		<title>Graydons Web Design</title>
 		<!-- <link rel="stylesheet" type="text/css" href="css/styles.css" />-->
 		<link rel="stylesheet" href="css/foundation.css" />
+		<link rel="stylesheet" href="webicons.css" />
+		<link rel="stylesheet" href="styles.css" />
 		<script src="js/modernizr.js"></script>
 	</head>
 	
@@ -47,7 +49,33 @@
 		<div class="row">
 			<div id="content">
 				<div class="large-12 columns">
-					<p>Here is an example homepage.</p>
+					<p>Welcome to my portfolio site.</p>
+					<p>Graydon Armstrong, Web Design Extraordinaire.</p>
+					<p>I am a Programming Analyst Student who aspires to run my own business. I want to do web design and programming for clients that is regarded as professional and of high quality. I enjoy programming and the challenges that it gives me from day to day.</p>
+					<a href="#" class="button">More Work</a>
+					<ul class="projects-slider" data-orbit>
+						<li>
+							<img src="https://forge-allura.apache.org/p/allura/wiki/Features/attachment/code_browsing.png" alt="slide 1" />
+							<div class="orbit-caption">
+							  Caption One.
+							</div>
+						</li>
+						<li>
+							<img src="http://www.securelist.com/en/images/pictures/klblog/681.png" alt="slide 2" />
+							<div class="orbit-caption">
+							  Caption Two.
+							</div>
+						</li>
+						<li>
+							<img src="http://code.snapstream.com/mediawiki/images/0/0d/02_SourceCode.png" alt="slide 3" />
+							<div class="orbit-caption">
+							  Caption Three.
+							</div>
+						</li>
+					</ul>
+					<a href="https://www.facebook.com/graydon.arm" class="webicon facebook">Like us on Facebook</a>
+					<a href="https://twitter.com/Graydon_Arm" class="webicon twitter">Follow us on Twitter</a>
+					<a href="#" class="webicon linkedin">Connect to me on LinkedIn</a>
 				</div>
 			</div>
 		</div>
@@ -55,7 +83,7 @@
 		<div class="row">
 			<div id="footer">
 				<div class="large-12 columns">
-					<p>&copy Graydons Web Design</p>
+					<p>Copyright Graydons Web Design 2014</p>
 				</div>
 			</div>
 		</div>
