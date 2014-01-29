bfe86fe3ddad4253087c5c78606b9a4b72c560a2
Graydon Armstrong <seeds77222@hotmail.com>
2014-01-29 01:32:12 -0500

    Seperated the head element into a seperate file for easy usability

---
+++ head.php
@@ -0,0 +1,10 @@
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Graydons Web Design</title>
+	<!-- <link rel="stylesheet" type="text/css" href="css/styles.css" />-->
+	<link rel="stylesheet" href="css/foundation.css" />
+	<link rel="stylesheet" href="webicons.css" />
+	<script src="js/modernizr.js"></script>
+	<link rel="stylesheet" href="css/styles.css" />
+</head>
