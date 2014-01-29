6ad6520b1770766b4f33db0931848a62bd68cf8b
Graydon Armstrong <seeds77222@hotmail.com>
2014-01-29 14:17:41 -0500

    made the topbar stick by fixing some css and adding a tag

--- header.php
+++ header.php
@@ -1,30 +1,30 @@
 <div class="row" id="header_logo">
-			</br>
-			<div class="large-12 columns">
-			<img src="img/logo.png" width=64 height=64>
-			</div>
-		</div>
-		
-		<div class="row">
-			<!--<div class="large-12 columns">-->
-				<nav class="top-bar" data-topbar>
-					<ul class="title-area">
-						<li class="name">
-							<h1><a href="#">Graydons Web Design</a></h1>
-						</li>
-						<li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
-					</ul>
-								
-					<section class="top-bar-section" id="nav_links">
-					<ul class="right">
-						<li class="active"><a href="index.php">Home</a></li>
-						<li class="active"><a href="aboutme.php">About Me</a></li>
-						<li class="active"><a href="#">Projects</a></li>
-						<li class="active"><a href="#">Services</a></li>
-						<li class="active"><a href="https://github.com/graydon-armstrong">Github</a></li>
-						<li class="active"><a href="#">Contact Me</a></li>
-					</ul>
-					</section>
-				</nav>
-			<!--</div>-->
+		</br>
+	<div class="large-12 columns">
+		<img src="img/logo.png" width=64 height=64>
+	</div>
+</div>
+	
+<div class="row">
+	<div class="contain-to-grid sticky">	
+		<nav class="top-bar" data-topbar>
+			<ul class="title-area">
+				<li class="name">
+					<h1><a>Graydons Web Design</a></h1>
+				</li>
+				<li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+			</ul>
+						
+			<section class="top-bar-section" id="nav_links">
+			<ul class="right">
+				<li class="active"><a href="index.php">Home</a></li>
+				<li class="active"><a href="aboutme.php">About Me</a></li>
+				<li class="active"><a href="#">Projects</a></li>
+				<li class="active"><a href="#">Services</a></li>
+				<li class="active"><a href="https://github.com/graydon-armstrong">Github</a></li>
+				<li class="active"><a href="#">Contact Me</a></li>
+			</ul>
+			</section>
+		</nav>
+	</div>
 </div>
