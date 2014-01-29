c3d752e77e94eb546965f9b97a11b4173739dd78
Graydon Armstrong <seeds77222@hotmail.com>
2014-01-29 15:01:55 -0500

    Added an image to my aboutme page

--- aboutme.php
+++ aboutme.php
@@ -13,15 +13,19 @@
 		<div class="row">
 			<div class="large-12 columns">
 				<h1>About Me</h1>
+				<hr />
 			</div>
 		</div>
 
 		<div class="row">
 			<div id="content">
-				<div class="large-12 columns">
+				<div class="medium-9 columns">
 					<p>Welcome to my portfolio site.</p>
 					<p>Graydon Armstrong, Web Design Extraordinaire.</p>
 					<p>I am a Programming Analyst Student who aspires to run my own business. I want to do web design and programming for clients that is regarded as professional and of high quality. I enjoy programming and the challenges that it gives me from day to day.</p>
+				</div>
+				<div class="medium-3 columns">
+					<img id="self_image" src="img/self_image.JPG" width=160 >
 				</div>
 			</div>
 		</div>
--- index.php
+++ index.php
@@ -12,7 +12,8 @@
 		
 		<div class="row">
 			<div class="large-12 columns">
-				<h1>Graydons Web Design</h1>
+				<h1>Home Page</h1>
+				<hr />
 			</div>
 		</div>
 
