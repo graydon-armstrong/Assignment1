0d705842e0088f8f5b1d48077a1962119bab7037
Graydon Armstrong <seeds77222@hotmail.com>
2014-01-30 00:22:24 -0500

    Completed the project descriptions

--- projects.php
+++ projects.php
@@ -21,7 +21,7 @@
 		<div class="row">
 			<div class="small-9 columns">				
 				
-				<p>I offer web design services that range from static sites, to content management systems and e-commerce websites. With meticulous work put into design and functionality, websites produced by our company will be engaging and memorable. We are competent in all current web design practices such as HTML5 and responsive design, which will help us to deliver a long lasting website with broad appeal.</p>
+				<p>This is a lab that we worked on in class to learn how to do a basic interactive flash page. It had us using lots of motion tweens and manipulating images to make the page interative.</p>
 			</div>
 			<div class="small-3 columns">
 				<a href="animation.html"><img class="shadow_image" src="img/animation.png" width=256 height=256></a>
@@ -37,7 +37,7 @@
 		<div class="row">
 			<div class="small-9 columns">				
 				
-				<p>We offer programming and development services to add functionality to your existing websites. We are willing to expand a static website to include e-commerce or other services to help expand your business into the global market. We are also able to develop web applications that you can use to run your business.</p>
+				<p>This assignment for my personal finance class has us keeping track of all our expenses so that we can learn how to budget. This is our first month of tracking, so we dont yet have an idea of how our spending will change from month to month yet. The next few months of tracking will teach us how to monitor our spending and budget approprietly for the next month.</p>
 			</div>
 			<div class="small-3 columns">
 				<a href="https://www.dropbox.com/s/u1y8kt4wglu5k6d/January%20Expense%20tracker.xlsx"><img class="shadow_image" src="img/finance.png" width=256 height=256></a>
@@ -53,7 +53,7 @@
 		<div class="row">
 			<div class="small-9 columns">				
 				
-				<p>We offer branding services to help bring a single image for your entire company. We will design a brand for you that will be in sync from your website to your business cards.</p>
+				<p>This is a graphics programming assignment from last semester where we had to make a small game in python. I decided to try and recreate space invaders with the twist of different classes of enemies that are each harder to kill than the last. We used python with the help of Aptana Studio 3 to create our games with the help of the pygame framework.</p>
 			</div>
 			<div class="small-3 columns">
 				<a href="https://github.com/graydon-armstrong/FinalProject"><img class="shadow_image" src="img/pythongame.png" width=256 height=256></a>
