e59ada6fc366887842fe86cb3b5e2e6203bbe566
Graydon Armstrong <seeds77222@hotmail.com>
2014-01-29 22:46:15 -0500

    Added begining of project page

---
+++ projects.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+	<?php include('head.php'); ?>
+	
+	<body>
+	
+	<div class="row" id= "main_layout">
+		<div class="large-12 columns">
+		
+		<?php include('header.php'); ?>
+		
+		<div class="row">
+			<div class="large-12 columns">
+				<h1>Projects</h1>
+				<hr />
+				<h4>2D Animation Tools</h4>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="small-9 columns">				
+				
+				<p>I offer web design services that range from static sites, to content management systems and e-commerce websites. With meticulous work put into design and functionality, websites produced by our company will be engaging and memorable. We are competent in all current web design practices such as HTML5 and responsive design, which will help us to deliver a long lasting website with broad appeal.</p>
+			</div>
+			<div class="small-3 columns">
+				<img class="shadow_image" src="img/animation.png" width=256 height=256>
+			</div>			
+		</div>
+		
+		<div class="row">
+			<div class="large-12 columns">
+				<h4>Personal Finance</h4>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="small-9 columns">				
+				
+				<p>We offer programming and development services to add functionality to your existing websites. We are willing to expand a static website to include e-commerce or other services to help expand your business into the global market. We are also able to develop web applications that you can use to run your business.</p>
+			</div>
+			<div class="small-3 columns">
+				<img class="shadow_image" src="img/finance.png" width=256 height=256>
+			</div>			
+		</div>
+		
+		<div class="row">
+			<div class="large-12 columns">
+				<h4>Graphics Programming</h4>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="small-9 columns">				
+				
+				<p>We offer branding services to help bring a single image for your entire company. We will design a brand for you that will be in sync from your website to your business cards.</p>
+			</div>
+			<div class="small-3 columns">
+				<a href="https://github.com/graydon-armstrong/FinalProject"><img class="shadow_image" src="img/pythongame.png" width=256 height=256></a>
+			</div>			
+		</div>
+
+		<?php include('footer.php') ?>
+		
+		</div>
+	</div>
+	
+	<script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+	
+
+	</body>
+
+</html>
