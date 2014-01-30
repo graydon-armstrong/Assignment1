b84fb0ec1629f3438e7a0314a5cc3ec4b8f8e9bb
Graydon Armstrong <seeds77222@hotmail.com>
2014-01-29 21:27:41 -0500

    Added start of services page

---
+++ services.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+	<?php include('head.php'); ?>
+	
+	<body>
+	
+	<div class="row" id= "main_layout">
+		<div class="large-12 columns">
+		
+		<?php include('header.php'); ?>
+		
+		<div class="row">
+			<div class="large-12 columns">
+				<h1>Services</h1>
+				<hr />
+				<h4>Web Design</h4>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="small-9 columns">				
+				
+				<p>I offer web design services that range from static sites, to content management systems and e-commerce websites. With meticulous work put into design and functionality, websites produced by our company will be engaging and memorable. We are competent in all current web design practices such as HTML5 and responsive design, which will help us to deliver a long lasting website with broad appeal.</p>
+			</div>
+			<div class="small-3 columns">
+				<img class="shadow_image" src="img/webdesign.jpg" width=256 height=256>
+			</div>			
+		</div>
+
+		<?php include('footer.php') ?>
+		
+		</div>
+	</div>
+	
+	<script src="js/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+	
+
+	</body>
+
+</html>
